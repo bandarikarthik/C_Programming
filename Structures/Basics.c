2c69108a9ce4d7976d73b94daa50fd5f7fb37e2b
bandarikarthik <93322915+bandarikarthik@users.noreply.github.com>
2022-08-06 10:43:56 +0530

    Update Basics.c

--- Structures/Basics.c
+++ Structures/Basics.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+struct book{
+	char name[20];
+	int  price;
+}machines={"ps bimbra",1500};
+
+
+int main(void){
+	struct book power_systems={"VK metha",750};
+	printf("%s %d\n",machines.name,machines.price);
+	printf("%s %d\n",power_systems.name,power_systems.price);
+	
+}
