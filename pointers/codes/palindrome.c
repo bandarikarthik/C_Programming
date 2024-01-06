e585e07811441d749cad94a0af1ca24b25975500
bandarikarthik <93322915+bandarikarthik@users.noreply.github.com>
2024-01-06 10:40:29 +0530

    Update palindrome.c

--- pointers/codes/palindrome.c
+++ pointers/codes/palindrome.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+#include <conio.h>
+#include<stdint.h>
+
+char *p = "malayalam";
+char *start,*end;
+uint16_t length;
+int main(){
+
+start = p; end = p;
+for( ; *end !='\0';end++ )length++;
+printf("%d\n",length);
+while(*start){
+	if(*start++ !=  *--end){
+		printf("Not a palindrome\n");
+		getch();
+		return 0;
+	}
+
+}
+printf("palindrome\n");
+getch();
+return 0;
+
+}
 
