b5595b252da5287c278ff864f0b0ae8ef142bd32
bandarikarthik <93322915+bandarikarthik@users.noreply.github.com>
2024-08-03 15:10:27 +0530

    Update 5.mystrcmp.c

--- Strings/PreDefinedFun/5.mystrcmp.c
+++ Strings/PreDefinedFun/5.mystrcmp.c
@@ -1,1 +1,25 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int mystrcmp(const char* str1, const char* str2);
 
+int main() {
+ char *str1 = "apple";
+ char *str2 = "banana";
+int result = mystrcmp(str1,str2);
+printf("%d",result);
+    return 0;
+}
+
+int mystrcmp(const char* str1, const char* str2){
+    
+  int result;
+    while(*str1++  && *str2++ ){
+        
+        if(*str1 == *str2)
+            result = 0;
+    else  if(*str1 < *str2)result = -1;
+      else  if(*str1 > *str2)result = 1;   
+    
+    }
+   return  result; 
+}
