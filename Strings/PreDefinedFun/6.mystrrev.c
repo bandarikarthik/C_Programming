c614a468b79da365f3ca6aa8f9c6c1248f93b846
bandarikarthik <93322915+bandarikarthik@users.noreply.github.com>
2024-08-05 14:06:31 +0530

    Update 6.mystrrev.c

--- Strings/PreDefinedFun/6.mystrrev.c
+++ Strings/PreDefinedFun/6.mystrrev.c
@@ -1,1 +1,35 @@
+/******************************************************************************
 
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+char *mystrrev(char *);
+int main()
+{
+ char p[10] = "karthik";  
+  printf("%s\n",mystrrev(p));
+  
+    return 0;
+}
+
+char *mystrrev(char *p){
+
+int start = 0,end = strlen(p)-1;
+char temp;
+while(start < end){
+
+temp = p[end];
+p[end] = p[start];
+p[start] = temp;
+start++;
+end--;
+
+}
+
+return p;
+}
