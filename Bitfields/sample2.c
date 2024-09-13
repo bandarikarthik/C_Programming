cdd6e3124093c13d16f21e7afb70cf456895a6f1
bandarikarthik <93322915+bandarikarthik@users.noreply.github.com>
2024-09-13 13:58:59 +0530

    Create sample2.c

---
+++ Bitfields/sample2.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include<stdint.h>
+
+struct spacket{
+
+ uint32_t  crc          :2;          //2  bit 
+ uint32_t  status       :1;       //1  bit 
+ uint32_t payload       :12;      //12 bit
+ uint32_t  bat          :3;          //3  bit
+ uint32_t  sensor       :3;       //3  bit
+ uint32_t  longaddr     :8;     //8  bit
+ uint32_t  shortaddr    :2;    //2  bit
+ uint32_t  addrmode     :1;     //1  bit
+
+};
+
+int main()
+{
+uint32_t packetvalue;
+struct spacket packet;
+
+printf("Enter packet value: ");scanf("%X",&packetvalue); //HEX format value
+
+packet.crc = (uint8_t)packetvalue & 0x3;   //last 2 bits
+//packet.status = (uint8_t)packetvalue & 0x4; 
+//your mistake isthat you can acess 2nd bit but how will you elobrate  
+
+packet.status = (uint8_t)((packetvalue >> 2) & 0x1);
+packet.payload = ( uint16_t)((packetvalue >> 3) & 0xFFF);
+packet.bat =        (uint8_t)((packetvalue >> 15) & 0x7);
+packet.sensor =     (uint8_t)((packetvalue >> 18) & 0x7);
+packet.longaddr = (uint8_t) ((packetvalue >> 21) & 0xFF);
+packet.shortaddr = (uint8_t) ((packetvalue >> 29) & 0x3);
+packet.addrmode = (uint8_t) ((packetvalue >> 31) & 0x1);
+
+printf("packet.crc %#X\n",packet.crc);
+printf("packet.status %#X\n",packet.status);
+printf("packet.payload %#X\n",packet.payload);
+
+printf("packet.bat %#X\n",packet.bat);
+printf("packet.sensor %#X\n",packet.sensor);
+
+
+printf("packet.longaddr%#X\n",packet.longaddr);
+
+printf("packet.shortaddr%#X\n",packet.shortaddr);
+printf("packet.addrmode %#X\n",packet.addrmode);
+
+printf("size of structure %ld\n", sizeof(struct spacket));
+    return 0;
+}
+
+
+
+/*
+
+Enter packet value: abcdefaa
+packet.crc 0X2
+packet.status 0
+packet.payload 0XDF5
+packet.bat 0X3
+packet.sensor 0X3
+packet.longaddr0X5E
+packet.shortaddr0X1
+packet.addrmode 0X1
+size of structure 4
+
+*/
